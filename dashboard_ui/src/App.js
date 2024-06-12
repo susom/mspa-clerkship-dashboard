b2c5c4f752b94cb2c8f01821502cd9c51830be7f
Irvin Szeto <irvins@stanford.edu>
2024-06-12 09:27:05 -0700

    feedback updates to the mspa dashboard

--- dashboard_ui/src/App.js
+++ dashboard_ui/src/App.js
@@ -1,20 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Tooltip } from 'react-tippy';
 import 'react-tippy/dist/tippy.css';
-
-import { Link, FileEarmarkText, FileEarmarkPlus, PersonCheck, Calendar, Envelope, ClipboardHeart, JournalMedical, PersonRolodex, ClipboardData, ListCheck, GraphUp } from 'react-bootstrap-icons';
-
+import { Link, FileEarmarkText, FileEarmarkPlus, PersonCheck, Envelope, ClipboardHeart, JournalMedical, PersonRolodex, GraphUp } from 'react-bootstrap-icons';
 import './App.css';
 
+// Function to determine the CSS class based on rotation data and criteria
 const getCssClassForRotation = (rotationData, isAdminView) => {
-    let result = {
-        className: '',
-        criteria: [],
-        data: {}
-    };
+    let result = { className: '', criteria: [], data: {} };
 
     const {
-        site_confirmation_complete,
         student_evaluation_of_preceptor_complete,
         preceptor_evals = [],
         communication_forms_complete,
@@ -35,30 +29,14 @@ const getCssClassForRotation = (rotationData, isAdminView) => {
     const rotationEnd = new Date(startDate);
     rotationEnd.setDate(rotationEnd.getDate() + 17); // Add 17 days to start date
 
-    // Keep the preceptor evaluations check as is
     const hasCompletePreceptorEvaluation = preceptor_evals.some(evaluation =>
-        evaluation.internal_medicine_i_complete === '2' ||
-        evaluation.internal_medicine_ii_complete === '2' ||
-        evaluation.primary_care_i_complete === '2' ||
-        evaluation.primary_care_ii_complete === '2' ||
-        evaluation.pediatrics_complete === '2' ||
-        evaluation.surgery_complete === '2' ||
-        evaluation.emergency_medicine_complete === '2' ||
-        evaluation.womens_health_complete === '2' ||
-        evaluation.behavioral_medicine_complete === '2' ||
-        evaluation.electives_complete === '2'
+        ['internal_medicine_i_complete', 'internal_medicine_ii_complete', 'primary_care_i_complete', 'primary_care_ii_complete', 'pediatrics_complete', 'surgery_complete', 'emergency_medicine_complete', 'womens_health_complete', 'behavioral_medicine_complete', 'electives_complete']
+            .some(key => evaluation[key] === '2')
     );
 
-    // Check for Aquifer case completion
-    const hasCompleteAquiferCase = aquifer302 === '1' ||
-        aquifer304 === '1' ||
-        aquifer311 === '1' ||
-        aquifer_other === '1' ||
-        specialty === 'SURG' ||
-        specialty === 'ELV'; // These specialties don't require Aquifer cases
+    const hasCompleteAquiferCase = ['aquifer302', 'aquifer304', 'aquifer311', 'aquifer_other'].some(key => rotationData[key] === '1') || ['SURG', 'ELV'].includes(specialty);
 
-    // Flag definitions
-    let flags = {
+    const flags = {
         rotationEndPassed: rotationEnd < today,
         studentEvalComp: student_evaluation_of_preceptor_complete === '2',
         preceptorEvalComp: hasCompletePreceptorEvaluation,
@@ -68,26 +46,23 @@ const getCssClassForRotation = (rotationData, isAdminView) => {
         commFormsComp: communication_forms_complete === '2',
         noFailEor: fail_eor === '0',
         eorRepeatScoreLow: eor_repeat_score && eor_repeat_score < 380,
-        siteConfirmed: site_confirmation_complete === '2',
         startDatePassed: startDate < today,
         startDateOrEqual: startDate <= today,
     };
 
-    if (flags.rotationEndPassed && flags.studentEvalComp && flags.preceptorEvalComp && flags.aquiferCaseComp &&
-        flags.patientLogComp && flags.avgScoreHigh && flags.commFormsComp && flags.noFailEor) {
+    // Determine class name and criteria based on flags
+    if (flags.rotationEndPassed && flags.studentEvalComp && flags.preceptorEvalComp && flags.aquiferCaseComp && flags.patientLogComp && flags.avgScoreHigh && flags.commFormsComp && flags.noFailEor) {
         result.className = 'bg_rotationend';
-        result.criteria.push('rotationEndPassed', 'studentEvalComp', 'preceptorEvalComp', 'aquiferCaseComp', 'patientLogComp', 'avgScoreHigh', 'commFormsComp', 'noFailEor');
-    } else if (flags.startDatePassed && ((flags.studentEvalComp && flags.preceptorEvalComp && flags.aquiferCaseComp &&
-        flags.patientLogComp && (!flags.avgScoreHigh || !flags.commFormsComp)) || flags.noFailEor)) {
+        result.criteria.push('Rotation Ended', 'SEP Complete', 'PES Complete', 'Aquifer Complete', 'Patient Log', 'PES Satisfactory', 'Feedback Sent', 'EOR Passed');
+    } else if (flags.startDatePassed && ((flags.studentEvalComp && flags.preceptorEvalComp && flags.aquiferCaseComp && flags.patientLogComp && !flags.commFormsComp) || flags.noFailEor)) {
         result.className = 'bg_rotationongoing';
-        result.criteria.push('startDatePassed', 'studentEvalComp', 'preceptorEvalComp', 'aquiferCaseComp', 'patientLogComp', 'avgScoreHigh', 'commFormsComp', 'noFailEor');
-    } else if (flags.startDateOrEqual && (!flags.studentEvalComp || !flags.preceptorEvalComp || !flags.aquiferCaseComp ||
-        !flags.patientLogComp || flags.eorRepeatScoreLow)) {
+        result.criteria.push('Rotation Started', 'SEP Complete', 'PES Complete', 'Aquifer Complete', 'Patient Log', 'PES Satisfactory', 'Feedback Sent', 'EOR Passed');
+    } else if (flags.startDateOrEqual && (!flags.studentEvalComp || !flags.preceptorEvalComp || !flags.aquiferCaseComp || !flags.patientLogComp || flags.eorRepeatScoreLow)) {
         result.className = 'bg_rotationstart';
-        result.criteria.push('startDateOrEqual', 'studentEvalComp', 'preceptorEvalComp', 'aquiferCaseComp', 'patientLogComp', 'eorRepeatScoreLow');
-    } else if (flags.siteConfirmed && flags.startDatePassed) {
+        result.criteria.push('Rotation Started', 'SEP Complete', 'PES Complete', 'Aquifer Complete', 'Patient Log', 'EOR Retake');
+    } else if (flags.startDatePassed) {
         result.className = 'bg_complete';
-        result.criteria.push('siteConfirmed', 'startDatePassed');
+        result.criteria.push('Rotation Started');
     }
 
     if (!isAdminView) {
@@ -96,26 +71,24 @@ const getCssClassForRotation = (rotationData, isAdminView) => {
     }
 
     result.data = flags; // Consolidated condition checks for easy reference
-
     return result;
 };
-
 
 // Function to extract specialty abbreviation from record_id
 const getSpecialtyFromRecordId = (recordId) => {
     const parts = recordId.split('_');
-    return parts.length > 2 ? parts[2]  : "";
+    return parts.length > 2 ? parts[2] : "";
 };
 
 function App() {
     const [periodDates, setPeriodDates] = useState({});
     const [studentsData, setStudentsData] = useState({});
-    const [criteriaVisibility, setCriteriaVisibility] = useState({}); // State object to control the visibility of each criteria list
+    const [criteriaVisibility, setCriteriaVisibility] = useState({});
 
     const toggleCriteriaVisibility = (index) => {
         setCriteriaVisibility(prev => ({
             ...prev,
-            [index]: !prev[index] // Toggle the boolean value for the index
+            [index]: !prev[index]
         }));
     };
 
@@ -123,7 +96,6 @@ function App() {
         setPeriodDates(window.periodDates || {});
         setStudentsData(window.studentsData || {});
 
-        // Initialize criteria visibility state
         const initialVisibility = {};
         Object.keys(window.studentsData || {}).forEach((studentKey, studentIndex) => {
             window.studentsData[studentKey].forEach((_, periodIndex) => {
@@ -133,18 +105,14 @@ function App() {
         setCriteriaVisibility(initialVisibility);
     }, []);
 
-
     const queryParams = new URLSearchParams(window.location.search);
     const isAdminView = queryParams.get('view') === 'admin';
 
-    // Convert studentsData to an array for easier mapping and sorting
     let students = Object.keys(studentsData).map(studentKey => {
-        // Extract last name from the studentKey
         const lastName = studentKey.split('_')[1].split(',')[0].trim();
 
-        // Create an array of periods based on periodDates, filling missing data with defaults
         const studentPeriods = Object.keys(periodDates).map(periodKey => {
-            const month = periodKey.split(' ')[1]; // Extract month number from periodKey
+            const month = periodKey.split(' ')[1];
             const periodData = studentsData[studentKey].find(p => p.month === month) || {};
             return {
                 ...periodData,
@@ -155,14 +123,13 @@ function App() {
         });
 
         return {
-            key: studentKey, // Include the studentKey here
+            key: studentKey,
             name: studentsData[studentKey]?.[0]?.full_name || "Unknown",
-            lastName, // Add the extracted last name
+            lastName,
             periods: studentPeriods
         };
     });
 
-    // Sort the students array by last name
     students.sort((a, b) => a.lastName.localeCompare(b.lastName));
 
     return (
@@ -182,25 +149,21 @@ function App() {
                     </thead>
                     <tbody>
                     {students.map((student, studentIndex) => {
-                        const scheduleLink  = student.periods[0]?.student_schedule;
-                        const mailtoLink    = `mailto:${student.periods[0]?.email}?subject=Your 2024 - 2025 Rotation Schedule&body=See your schedule at: ${encodeURIComponent(scheduleLink)}`;
-                        console.log("student data", student.periods[0]);
+                        const scheduleLink = student.periods[0]?.student_schedule;
+                        const mailtoLink = `mailto:${student.periods[0]?.email}?subject=Your 2024 - 2025 Rotation Schedule&body=See your schedule at: ${encodeURIComponent(scheduleLink)}`;
 
-                        return(<tr key={studentIndex}>
+                        return (
+                            <tr key={studentIndex}>
                                 <td>
                                     <div>{student.name}</div>
                                     {(student.periods[0]?.student_url || student.periods[0]?.le_student_url) && (
                                         <div className={"student_links"}>
                                             <p>Evaluation Links</p>
                                             {student.periods[0]?.student_url && (
-                                                <a href={student.periods[0].student_url} target="_blank"
-                                                   rel="noopener noreferrer"
-                                                   title={"Clerkship Evaulations"}><PersonCheck/></a>
+                                                <a href={student.periods[0].student_url} target="_blank" rel="noopener noreferrer" title={"Clerkship Evaluations"}><PersonCheck /></a>
                                             )}
                                             {student.periods[0]?.le_student_url && (
-                                                <a href={student.periods[0].le_student_url} target="_blank"
-                                                   rel="noopener noreferrer"
-                                                   title={"Individual Lecture Evaluations"}><GraphUp/></a>
+                                                <a href={student.periods[0].le_student_url} target="_blank" rel="noopener noreferrer" title={"Individual Lecture Evaluations"}><GraphUp /></a>
                                             )}
                                         </div>
                                     )}
@@ -208,14 +171,10 @@ function App() {
                                         <div className={"student_links"}>
                                             <p>Onboarding Links</p>
                                             {student.periods[0]?.gen_onboarding_link && (
-                                                <a href={student.periods[0].gen_onboarding_link} target="_blank"
-                                                   rel="noopener noreferrer"
-                                                   title={"General Onboarding Docs"}><FileEarmarkText/></a>
+                                                <a href={student.periods[0].gen_onboarding_link} target="_blank" rel="noopener noreferrer" title={"General Onboarding Docs"}><FileEarmarkText /></a>
                                             )}
                                             {student.periods[0]?.addl_onboarding_link && (
-                                                <a href={student.periods[0].addl_onboarding_link} target="_blank"
-                                                   rel="noopener noreferrer"
-                                                   title={"Additional Onboarding Docs"}><FileEarmarkPlus/></a>
+                                                <a href={student.periods[0].addl_onboarding_link} target="_blank" rel="noopener noreferrer" title={"Additional Onboarding Docs"}><FileEarmarkPlus /></a>
                                             )}
                                         </div>
                                     )}
@@ -232,16 +191,12 @@ function App() {
 
                                 {student.periods.map((period, index) => {
                                     const cssInfo = getCssClassForRotation(period, isAdminView);
-                                    const hasRotationData = period.location || period.specialty; // Add more checks as needed
-                                    const showCriteria = criteriaVisibility[`${studentIndex}_${index}`]; // This should be the state that controls visibility
+                                    const hasRotationData = period.location || period.specialty;
+                                    const showCriteria = criteriaVisibility[`${studentIndex}_${index}`];
 
                                     return (
                                         <td key={index} className={cssInfo.className}>
-                                            <Tooltip
-                                                title={period.siteAddress}
-                                                position="top"
-                                                key={index}
-                                            >
+                                            <Tooltip title={period.siteAddress} position="top" key={index}>
                                                 {period.locationParts.map((part, partIndex) => (
                                                     <div key={partIndex}>
                                                         {part}{partIndex < period.locationParts.length - 1 ? '' : ''}
@@ -253,27 +208,19 @@ function App() {
                                                 <div className={`student_links`}>
                                                     <p>Rotation Links</p>
                                                     {period.onboarding_link && (
-                                                        <a href={period.onboarding_link} target="_blank"
-                                                           rel="noopener noreferrer"
-                                                           title={"Rotation Onboarding"}><PersonRolodex/></a>
+                                                        <a href={period.onboarding_link} target="_blank" rel="noopener noreferrer" title={"Rotation Onboarding"}><PersonRolodex /></a>
                                                     )}
                                                     {period.cef_url && (
-                                                        <a href={period.cef_url} target="_blank"
-                                                           rel="noopener noreferrer"
-                                                           title={"Clerkship Expectations Form"}><JournalMedical/></a>
+                                                        <a href={period.cef_url} target="_blank" rel="noopener noreferrer" title={"Clerkship Expectations Form"}><JournalMedical /></a>
                                                     )}
                                                     {period.patient_log_url && (
-                                                        <a href={period.patient_log_url} target="_blank"
-                                                           rel="noopener noreferrer" title={"Patient Logs"}><ClipboardHeart/></a>
+                                                        <a href={period.patient_log_url} target="_blank" rel="noopener noreferrer" title={"Patient Logs"}><ClipboardHeart /></a>
                                                     )}
                                                 </div>
                                             )}
-                                            {(isAdminView && hasRotationData && cssInfo.data ) && (
+                                            {(isAdminView && hasRotationData && cssInfo.data) && (
                                                 <div className={`student_links`}>
-                                                    <p
-                                                        onClick={() => toggleCriteriaVisibility(`${studentIndex}_${index}`)}
-                                                        className={`status-criteria-toggle ${criteriaVisibility[`${studentIndex}_${index}`] ? 'expanded' : ''}`}
-                                                    >
+                                                    <p onClick={() => toggleCriteriaVisibility(`${studentIndex}_${index}`)} className={`status-criteria-toggle ${criteriaVisibility[`${studentIndex}_${index}`] ? 'expanded' : ''}`}>
                                                         Status Criteria
                                                     </p>
 
@@ -281,10 +228,7 @@ function App() {
                                                         <ul className="status_criteria">
                                                             {Object.entries(cssInfo.data).map(([key, value]) => (
                                                                 <li key={key}>
-                                                                    <a href="#!" className={cssInfo.criteria.includes(key) ? 'highlight_critera' : ''} onClick={(e) => {
-                                                                        e.preventDefault();
-                                                                        // Define your link click handler here
-                                                                    }}>
+                                                                    <a href="#!" className={cssInfo.criteria.includes(key) ? 'highlight_critera' : ''} style={{ color: value ? 'green' : 'red' }}>
                                                                         {`${key}: ${value}`}
                                                                     </a>
                                                                 </li>
@@ -293,20 +237,17 @@ function App() {
                                                     )}
                                                 </div>
                                             )}
-                                            {period.average_score && (
-                                                <span className="average-score">{period.average_score}</span>
-                                            )}
                                         </td>
                                     );
                                 })}
                             </tr>
-                        )})}
+                        );
+                    })}
                     </tbody>
                 </table>
             </div>
         </div>
     );
-
 }
 
 export default App;
